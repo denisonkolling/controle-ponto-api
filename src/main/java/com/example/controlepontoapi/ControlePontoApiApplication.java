2572d99ff61433119dbb11bf7fb963a68402c6b5
Denison Kolling <denisonkolling@gmail.com>
2023-11-26 07:32:48 -0300

    Ex 01 - Criacao projeto

---
+++ src/main/java/com/example/controlepontoapi/ControlePontoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.controlepontoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlePontoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControlePontoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/controlepontoapi/ControlePontoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controlepontoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlePontoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
