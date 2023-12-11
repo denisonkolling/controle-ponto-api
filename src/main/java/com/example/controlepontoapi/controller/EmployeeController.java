15a057ac32ea5e85755f6196fa7e9433d9a91b8a
Denison Kolling <denisonkolling@gmail.com>
2023-12-11 18:09:58 -0300

    Ex 06 - Endpoint listagem funcionarios

--- src/main/java/com/example/controlepontoapi/controller/EmployeeController.java
+++ src/main/java/com/example/controlepontoapi/controller/EmployeeController.java
@@ -5,12 +5,13 @@ import com.example.controlepontoapi.dto.EmployeeResponseDTO;
 import com.example.controlepontoapi.service.EmployeeService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/employees")
@@ -24,4 +25,11 @@ public class EmployeeController {
         EmployeeResponseDTO response = this.employeeService.create(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+
+    @GetMapping
+    public ResponseEntity<Page<EmployeeResponseDTO>> list(@PageableDefault(size = 12, sort = "name") Pageable pageable) {
+        Page<EmployeeResponseDTO> response = this.employeeService.listAll(pageable);
+        return ResponseEntity.ok(response);
+    }
+
 }
--- src/main/java/com/example/controlepontoapi/service/EmployeeService.java
+++ src/main/java/com/example/controlepontoapi/service/EmployeeService.java
@@ -4,8 +4,11 @@ import com.example.controlepontoapi.dto.EmployeeRequestDTO;
 import com.example.controlepontoapi.dto.EmployeeResponseDTO;
 import com.example.controlepontoapi.model.Employee;
 import com.example.controlepontoapi.repository.EmployeeRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class EmployeeService {
@@ -16,6 +19,10 @@ public class EmployeeService {
     public EmployeeResponseDTO create(EmployeeRequestDTO request) {
         Employee newEmployee = this.employeeRepository.save(new Employee(request));
         return new EmployeeResponseDTO(newEmployee);
+    }
+
+    public Page<EmployeeResponseDTO> listAll(Pageable pageable) {
+        return this.employeeRepository.findAll(pageable).map(EmployeeResponseDTO::new);
     }
 
 }
