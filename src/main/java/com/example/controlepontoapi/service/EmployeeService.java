ff935c70c772f2dbc087570145bfeff8d68f7a96
Denison Kolling <denisonkolling@gmail.com>
2023-12-12 10:53:30 -0300

    Ex 09 - Documentacao e Logs

--- src/main/java/com/example/controlepontoapi/service/EmployeeService.java
+++ src/main/java/com/example/controlepontoapi/service/EmployeeService.java
@@ -5,6 +5,8 @@ import com.example.controlepontoapi.model.Employee;
 import com.example.controlepontoapi.model.Register;
 import com.example.controlepontoapi.repository.EmployeeRepository;
 import com.example.controlepontoapi.repository.RegisterRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmployeeService {
 
+    private final Logger logger = LoggerFactory.getLogger(EmployeeService.class);
+
     @Autowired
     EmployeeRepository employeeRepository;
 
@@ -22,6 +26,7 @@ public class EmployeeService {
 
     public EmployeeResponseDTO createEmployee(EmployeeRequestDTO request) {
         Employee newEmployee = this.employeeRepository.save(new Employee(request));
+        logger.info("Employee created: {}", newEmployee);
         return new EmployeeResponseDTO(newEmployee);
     }
 
@@ -30,18 +35,25 @@ public class EmployeeService {
     }
 
     public EmployeeRegistersResponseDTO getEmployee(Long id) {
+        logger.info("Getting employee by id: {}", id);
         return this.employeeRepository.findById(id).map(EmployeeRegistersResponseDTO::new)
-                .orElseThrow(() -> new IllegalArgumentException("Employee with id not found: " + id));
+                .orElseThrow(() -> {
+                    logger.error("Employee with id not found: {}", id);
+                    return new IllegalArgumentException("Employee with id not found: " + id);
+                });
     }
 
     public RegisterResponseDTO createRegister(Long id, RegisterRequestDTO request) {
         Employee employee = this.employeeRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("Employee with id not found: " + id));
+                .orElseThrow(() -> {
+                    logger.error("Employee with id not found: {}", id);
+                    return new IllegalArgumentException("Employee with id not found: " + id);
+                });
 
         Register register = new Register(request.type(), employee);
 
         this.registerRepository.save(register);
-
+        logger.info("Register created: {}", register);
         return new RegisterResponseDTO(register);
 
     }
